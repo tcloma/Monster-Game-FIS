7f8041dcdac3df15ea97597360087ff801513635
Tyrone <tyrone.cloma0219@gmail.com>
2022-07-12 10:44:47 -0400

    HTML, CSS, JS backup

---
+++ index.js
@@ -0,0 +1,74 @@
+const pixelCanvas = document.querySelector('.canvas')
+const pColorInput = document.querySelector('#pColor-input')
+const sColorInput = document.querySelector('#sColor-input')
+const generateButton = document.querySelector('#generate-monster-button')
+const fillDropDown = document.querySelector('#fill-type')
+const randomButton = document.querySelector('#random-monster')
+
+let primaryColor
+let secondaryColor
+let filltype
+
+let monsterUrl = `https://app.pixelencounter.com/api/basic/svgmonsters/json?primaryColor=%23${primaryColor}&secondarycColor=%23${secondaryColor}&fillType=${filltype}`
+
+let renderPixels = (data) => {
+    pixelCanvas.innerHTML = ""
+
+    let localPattern = [];
+    let colorValue = data;
+    let boxCount = 0;
+
+    colorValue.forEach((element, i) => {
+        localPattern.push(element)
+        console.log(`Pattern ${i}:`, localPattern[i])
+
+        localPattern[i].forEach((element) => {
+            console.log(element)
+            let pixel = document.createElement('div')
+            pixel.className = "pixel"
+
+            let span = document.createElement('span')
+            boxCount++;
+            span.innerText = boxCount
+            pixel.append(span)
+
+            if (element === 1) {
+                pixel.style.backgroundColor = primaryColor
+            }
+            else if (element === 2) {
+                pixel.style.backgroundColor = secondaryColor
+            }
+
+            pixelCanvas.append(pixel)
+        })
+    })
+}
+
+const fetchData = async () => {
+    let req = await fetch(monsterUrl)
+    let res = await req.json()
+    renderPixels(res.pattern)
+}
+
+// const fetchRandom = async () => {
+//     let req = await fetch('https://app.pixelencounter.com/api/basic/monsters/random/json')
+//     let res = await req.json()
+//     console.log(res)
+// }
+
+generateButton.addEventListener('click', () => {
+    primaryColor = pColorInput.value
+    secondaryColor = sColorInput.value
+    filltype = fillDropDown.value
+
+    monsterUrl = `https://app.pixelencounter.com/api/basic/svgmonsters/json?primaryColor=%23${primaryColor}&secondarycColor=%23${secondaryColor}&fillType=${filltype}`
+
+    console.log(primaryColor, secondaryColor, filltype)
+    event.preventDefault()
+    fetchData()
+})
+
+// randomButton.addEventListener('click', () => {
+//     event.preventDefault()
+//     fetchRandom()
+// })
