69359266bfe76fd79adec96f1e74580bef121abb
Tyrone <tyrone.cloma0219@gmail.com>
2022-07-12 11:39:02 -0400

    Complete Basic Monster Rendering

--- index.js
+++ index.js
@@ -8,35 +8,31 @@ const randomButton = document.querySelector('#random-monster')
 let primaryColor
 let secondaryColor
 let filltype
-
-let monsterUrl = `https://app.pixelencounter.com/api/basic/svgmonsters/json?primaryColor=%23${primaryColor}&secondarycColor=%23${secondaryColor}&fillType=${filltype}`
+let monsterUrl
 
 let renderPixels = (data) => {
     pixelCanvas.innerHTML = ""
 
     let localPattern = [];
     let colorValue = data;
-    let boxCount = 0;
+    // let boxCount = 1;
 
     colorValue.forEach((element, i) => {
         localPattern.push(element)
-        console.log(`Pattern ${i}:`, localPattern[i])
+        // console.log(`Pattern ${i}:`, localPattern[i])
 
         localPattern[i].forEach((element) => {
-            console.log(element)
+            // console.log(element)
             let pixel = document.createElement('div')
             pixel.className = "pixel"
-
-            let span = document.createElement('span')
-            boxCount++;
-            span.innerText = boxCount
-            pixel.append(span)
+            // pixel.innerText = boxCount
+            // boxCount++;
 
             if (element === 1) {
-                pixel.style.backgroundColor = primaryColor
+                pixel.style.backgroundColor = `#${primaryColor}`
             }
             else if (element === 2) {
-                pixel.style.backgroundColor = secondaryColor
+                pixel.style.backgroundColor = `#${secondaryColor}`
             }
 
             pixelCanvas.append(pixel)
@@ -63,7 +59,7 @@ generateButton.addEventListener('click', () => {
 
     monsterUrl = `https://app.pixelencounter.com/api/basic/svgmonsters/json?primaryColor=%23${primaryColor}&secondarycColor=%23${secondaryColor}&fillType=${filltype}`
 
-    console.log(primaryColor, secondaryColor, filltype)
+    // console.log(primaryColor, secondaryColor, filltype, monsterUrl)
     event.preventDefault()
     fetchData()
 })
