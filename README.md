0d972a096e843067eb9c2087ec301a6b5dac0493
Tyrone <tyrone.cloma0219@gmail.com>
2022-07-12 16:40:41 -0400

    Random & Confirm button working

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# phase1-project-mosntergame
+Stuff to do: 
+
+- [x] Random Button
+- [x] Confirm Button
+    - Naming Monster
+    - Removing creation screen UI and replacing it with game UI
+
+Game mechanics:
+    - Monster Name
+    - Hunger
+    - Happiness 
+
+    - "Draining" hunger / Happiness using TIMER
+    - action to increase time TIMER duration
+        - for Happiness (cleaning poop that randomly pops up)
+        - for hunger (typing in a food?)
+
+    - Death sequence when bar is drained
+
+Design and styling: (try for pixel art)
+    - Basic UI for hunger and happiness
+        - Square buttons with bars on the side
+
+    - Basic bouncing animation for monster
+
+
+User Experience:
+    - Guide to color picking at the start
+    - Quick tutorial sequence going over what to press and how to play (ask if they want one)
--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 const pixelCanvas = document.querySelector('.canvas')
+const inputFields = document.querySelector('#input-fields')
 const pColorInput = document.querySelector('#pColor-input')
 const sColorInput = document.querySelector('#sColor-input')
-const generateButton = document.querySelector('#generate-monster-button')
 const fillDropDown = document.querySelector('#fill-type')
+const generateButton = document.querySelector('#generate-monster-button')
 const randomButton = document.querySelector('#random-monster')
+const confirmButton = document.querySelector('#confirm-monster')
+const monsNameContainer = document.querySelector('#mons-name')
 
 let primaryColor
 let secondaryColor
 let filltype
 let monsterUrl
+let monsterName
 
 let renderPixels = (data) => {
     pixelCanvas.innerHTML = ""
 
     let localPattern = [];
-    let colorValue = data;
+    let colorValue = data.pattern;
+    let colorGrab = data.colors;
     // let boxCount = 1;
 
     colorValue.forEach((element, i) => {
@@ -27,12 +32,18 @@ let renderPixels = (data) => {
             pixel.className = "pixel"
             // pixel.innerText = boxCount
             // boxCount++;
-
-            if (element === 1) {
-                pixel.style.backgroundColor = `#${primaryColor}`
+            if (monsterUrl !== 'https://app.pixelencounter.com/api/basic/monsters/random/json') {
+                if (element === 1) {
+                    pixel.style.backgroundColor = `#${primaryColor}`
+                }
+                else if (element === 2) {
+                    pixel.style.backgroundColor = `#${secondaryColor}`
+                }
             }
-            else if (element === 2) {
-                pixel.style.backgroundColor = `#${secondaryColor}`
+            else{
+                if (element === 1){
+                    pixel.style.backgroundColor = colorGrab[1]
+                }
             }
 
             pixelCanvas.append(pixel)
@@ -43,20 +54,19 @@ let renderPixels = (data) => {
 const fetchData = async () => {
     let req = await fetch(monsterUrl)
     let res = await req.json()
-    renderPixels(res.pattern)
+    renderPixels(res)
 }
 
-// const fetchRandom = async () => {
-//     let req = await fetch('https://app.pixelencounter.com/api/basic/monsters/random/json')
-//     let res = await req.json()
-//     console.log(res)
-// }
+const fetchRandom = async () => {
+    let req = await fetch('https://app.pixelencounter.com/api/basic/monsters/random/json')
+    let res = await req.json()
+    renderPixels(res)
+}
 
 generateButton.addEventListener('click', () => {
     primaryColor = pColorInput.value
     secondaryColor = sColorInput.value
     filltype = fillDropDown.value
-
     monsterUrl = `https://app.pixelencounter.com/api/basic/svgmonsters/json?primaryColor=%23${primaryColor}&secondarycColor=%23${secondaryColor}&fillType=${filltype}`
 
     // console.log(primaryColor, secondaryColor, filltype, monsterUrl)
@@ -64,7 +74,25 @@ generateButton.addEventListener('click', () => {
     fetchData()
 })
 
-// randomButton.addEventListener('click', () => {
-//     event.preventDefault()
-//     fetchRandom()
-// })
+randomButton.addEventListener('click', () => {
+    monsterUrl = 'https://app.pixelencounter.com/api/basic/monsters/random/json'
+    event.preventDefault()
+    fetchRandom()
+})
+
+confirmButton.addEventListener('click', () => {
+    let decision = confirm('Do you want to summon this monster?')
+    
+    if (decision === true){
+        console.log('Okay then!')
+        monsterName = prompt("Enter your monster's name!")
+        inputFields.remove()
+        monsNameContainer.textContent = monsterName
+        // Call new scene
+    }
+    else {
+        console.log('Take your time!')
+    }
+
+    event.preventDefault()
+})
